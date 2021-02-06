59f3c2d1eca41d360c43a9e71cea5a5b6db9060c
Hossain <hossainrabbi259@gmail.com>
2021-02-06 11:36:58 +0600

    bug fixed

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ fetch('https://restcountries.eu/rest/v2/all')
       const listItem = document.createElement('div');
       listItem.className = 'col-md-4';
       listItem.innerHTML = `
-            <div class="country-list">
-              <h3>${country.name}</h3>
-              <p>${country.capital}</p>
-              <button 
-                onclick="countryDetails('${country.name}')" 
-                class="btn btn-primary">Details
-              </button>
-            </div>
-            `;
+        <div class="country-list">
+          <h3>${country.name}</h3>
+          <p>${country.capital}</p>
+          <button 
+            onclick="countryDetails('${country.name}')" 
+            class="btn btn-primary">Details
+          </button>
+        </div>
+        `;
       countryContent.appendChild(listItem);
     });
   });
@@ -24,31 +24,29 @@ function countryDetails(name) {
     .then((res) => res.json())
     .then((data) => {
       const detailsArea = document.getElementById('details-area');
-      const countryDetails = document.createElement('div');
-      countryDetails.className = 'country-details';
-      countryDetails.innerHTML = `
-            <div class="container text-center country-content text-white">
-                <h2 class="mb-0">${data[0].name}</h2>
-                <h5 class="mb-0">Region: ${data[0].region}</h5>
-                <p class="mb-0">Capital: ${data[0].capital}</p>
-                <p class="mb-0">Population: ${data[0].population}</p>
-                <p class="mb-0">Area: ${data[0].area}</p>
-                <div>
-                    <img
-                    class="w-25"
-                    src="${data[0].flag}"
-                    alt="${data[0].name}-flag"
-                    />
-                </div>
-                <button onclick="closeBtn()" class="btn btn-danger mt-3">Close</button>
-            </div>
-            `;
-      detailsArea.appendChild(countryDetails);
+      detailsArea.innerHTML = `
+        <div class ="country-details d-flex justify-content-center align-items-center">
+          <div class="container text-center country-content text-white">
+              <h2 class="mb-0">${data[0].name}</h2>
+              <h5 class="mb-0">Region: ${data[0].region}</h5>
+              <p class="mb-0">Capital: ${data[0].capital}</p>
+              <p class="mb-0">Population: ${data[0].population}</p>
+              <p class="mb-0">Area: ${data[0].area}</p>
+              <div>
+                  <img
+                  class="w-25"
+                  src="${data[0].flag}"
+                  alt="${data[0].name}-flag"
+                  />
+              </div>
+              <button onclick="closeBtn()" class="btn btn-danger mt-3">Close</button>
+          </div>
+        <div>
+        `;
       detailsArea.className = 'd-block';
     });
 }
 
 function closeBtn() {
   document.getElementById('details-area').className = 'd-none';
-  document.querySelector('.country-details').style.display = 'none';
 }
